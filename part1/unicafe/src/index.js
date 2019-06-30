1b4515e5c616d81688bd47d3a0cc6275be349dda
Meike <meike_hankewicz@yahoo.de>
2019-06-30 22:30:09 +0200

    exercise 1.7 - 1.8

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -3,22 +3,19 @@ import ReactDOM from 'react-dom'
 
 const Button = ({text, onClick}) => <button onClick={onClick}>{text}</button>
 
-/* const Display = ({props}) => {
-  return(
-    <div>
-    {props.map(prop => (
-      <p>{prop}</p>
-    ))}
-    </div>
-  )
-} */
-
 const Statistics = ({good, neutral, bad}) => {
+  const all = good + neutral + bad
+  const average = (good * 1 + bad * -1) / all
+  const positive = good * 100 / all
+
   return(
     <div>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive}</p>
     </div>
   )
 }
