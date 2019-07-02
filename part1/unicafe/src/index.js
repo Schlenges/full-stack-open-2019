01d61160d859bf6d1cf9801d3b4c20ddc72a7edc
Meike <meike_hankewicz@yahoo.de>
2019-07-02 22:33:28 +0200

    unicafe state property changes

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -26,20 +26,24 @@ const Statistic = ({text, value}) => <tr><td>{text}</td><td>{value}</td></tr>
 
 
 const App = () => {
-  // save clicks of each button to own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const all = good + neutral + bad
+  const [all, setAll] = useState(0)
   const average = (good - bad) / all
   const positive = good * 100 / all
+
+  const handleClick = (setValue, value) => {
+    setValue(value + 1)
+    setAll(all + 1)
+  }
 
   return (
     <div>
       <h1>give feedback</h1>
-      <Button text="good" onClick={() => setGood(good+1)}/>
-      <Button text="neutral" onClick={() => setNeutral(neutral+1)}/>
-      <Button text="bad" onClick={() => setBad(bad+1)}/>
+      <Button text="good" onClick={() => handleClick(setGood, good)}/>
+      <Button text="neutral" onClick={() => handleClick(setNeutral, neutral)}/>
+      <Button text="bad" onClick={() => handleClick(setBad, bad)}/>
 
       <h1>statistics</h1>
       <Statistics good={good}
@@ -52,6 +56,4 @@ const App = () => {
   )
 }
 
-ReactDOM.render(<App />, 
-  document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById('root'))
