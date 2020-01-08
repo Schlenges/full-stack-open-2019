4b7443db727bb985c09a6f1e0476660ee48667a0
Schlenges <meike_hankewicz@yahoo.de>
2020-01-08 22:58:24 +0100

    exercise 4.4.

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  let sum = 0
+  blogs.map(blog => sum += blog.likes)
+  return sum
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
