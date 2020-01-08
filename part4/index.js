7ac41ad448eac372e72d1fe9f8aeb77eb6f51011
Schlenges <meike_hankewicz@yahoo.de>
2020-01-08 15:01:57 +0100

    exercise 4.1

---
+++ part4/index.js
@@ -0,0 +1,49 @@
+const http = require('http')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const mongoUrl = 'mongodb+srv://fullstack:hanki@fso-r04oo.mongodb.net/bloglist?retryWrites=true&w=majority'
+mongoose
+  .connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+    .catch(error => console.log(error))
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
